d86dd60aa368e7affcebac1c710a3e9584f9cf2d
cayomol01 <60375814+cayomol01@users.noreply.github.com>
2022-08-17 12:54:57 -0600

    middle - dutch shot

--- Engine3D.py
+++ Engine3D.py
@@ -2,17 +2,69 @@ from gl import Renderer, color, V3, V2
 from texture import Texture
 from shaders import flat, gourad
 
-width = 1024
-height = 800
+width = 960
+height = 540
 
 rend = Renderer(width, height)
-
 rend.active_shader = gourad
+rend.active_texture = Texture("models/bullTexture.bmp")
+
+rend.dirLight = V3(0.5, 0, -0.5)
+rend.glLoadModel("models/bull.obj",
+                 translate = V3(0, -1, -10),
+                 scale = V3(0.2,0.2,0.2),
+                 rotate = V3(0,35,0))
+
+rend.glFinish("Outputs/MiddleShot.bmp")
+
+#high angle
+''' rend.active_shader = gourad
 rend.active_texture = Texture("bullTexture.bmp")
 
+rend.dirLight = V3(0.5, 0, -0.5)
 rend.glLoadModel("bull.obj",
-                 translate = V3(0, 0, -10),
-                 scale = V3(0.01,0.01,0.01),
-                 rotate = V3(0,90,0))
+                 translate = V3(0, -1.7, -8),
+                 scale = V3(0.2,0.2,0.2),
+                 rotate = V3(-35,0,0))
+
+rend.glFinish("output.bmp") '''
+
+
+#Low angle
+''' rend.active_shader = gourad
+rend.active_texture = Texture("bullTexture.bmp")
+
+rend.dirLight = V3(0, -1, 0)
+rend.glLoadModel("bull.obj",
+                 translate = V3(0, -1.5, -7),
+                 scale = V3(0.3,0.3,0.3),
+                 rotate = V3(-30,0,0))
 
 rend.glFinish("output.bmp")
+ '''
+ 
+#middle shot
+'''  
+rend.active_shader = gourad
+rend.active_texture = Texture("models/bullTexture.bmp")
+
+rend.dirLight = V3(0.5, 0, -0.5)
+rend.glLoadModel("models/bull.obj",
+                 translate = V3(0, -1, -10),
+                 scale = V3(0.2,0.2,0.2),
+                 rotate = V3(0,35,0))
+
+rend.glFinish("Outputs/MiddleShot.bmp") '''
+
+#Dutch shot
+''' rend.active_shader = gourad
+rend.active_texture = Texture("models/bullTexture.bmp")
+
+rend.dirLight = V3(0.5, 0, -0.5)
+rend.glLoadModel("models/bull.obj",
+                 translate = V3(0, -1, -10),
+                 scale = V3(0.2,0.2,0.2),
+                 rotate = V3(-20,0,45))
+
+rend.glFinish("Outputs/DutchShot.bmp")
+ '''
